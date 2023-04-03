2b0af0f707ffd1c0934099f0750949e3d63ed6c0
vicotroshenko <otroshenko.vl@gmail.com>
2023-04-03 17:22:05 +0300

    +

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,12 @@
 import throttle from 'lodash.throttle';
-
+import Player from '@vimeo/player';
 
 const iframe = document.querySelector('iframe');
 
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
 player.on('timeupdate', throttle(function(data) {
-    const videoCurrentTime = data.seconds;
+    const videoCurrentTime = JSON.stringify(data.seconds);
     localStorage.setItem('videoplayer-current-time', videoCurrentTime)
 }, 1000));
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,30 +5,31 @@ const textarea = document.querySelector('.feedback-form textarea');
 const input = document.querySelector('.feedback-form input')
 
 
-const formData = {};
+let formData = {};
 form.addEventListener('submit', formSubmit);
 unsendedMessage();
 
 form.addEventListener('input', throttle(event => {
 	const savedMessageJSON = localStorage.getItem('feedback-form-state');
 	if(savedMessageJSON) {
-		const savedMessage = JSON.parse(savedMessageJSON);
-		savedMessage[event.target.name] = event.target.value;
-		const renewSavesMessageJSON = JSON.stringify(savedMessage);
+		formData = JSON.parse(savedMessageJSON);
+		formData[event.target.name] = event.target.value;
+		const renewSavesMessageJSON = JSON.stringify(formData);
 		localStorage.setItem('feedback-form-state', renewSavesMessageJSON);
 	} else {
 		formData[event.target.name] = event.target.value;
 		const formDataJSON = JSON.stringify(formData);
 		localStorage.setItem('feedback-form-state', formDataJSON);
 	}
-
+	
 }, 500));
 
 
 function formSubmit(event) {
+	const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
+	console.log(savedMessage)
 	event.preventDefault();
 	event.target.reset();
-	console.log(formData)
 	localStorage.removeItem('feedback-form-state');
 
 };
@@ -37,7 +38,6 @@ function formSubmit(event) {
 function unsendedMessage() {
 	const savedMessageJSON = localStorage.getItem('feedback-form-state');
 	const savedMessage = JSON.parse(savedMessageJSON)
-	console.log(savedMessage)
 	if(savedMessage) {
 		savedMessage.message !== undefined ? textarea.value = savedMessage.message : textarea.value = "";
 		savedMessage.email !== undefined ? input.value = savedMessage.email : input.value = "";
